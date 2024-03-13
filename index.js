ead1771d97d56fcb826d0e4766678f755bfde7cb
deepak <dgusaiwal537@gmail.com>
2024-03-13 21:16:10 +0530

    solve 3rd question of dom

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 let isClicked = false;
 document.querySelector(".button").addEventListener("click", () => {
-
-  !isClicked ? document.querySelector(".button").innerText +=' is starting..' : null
+  !isClicked
+    ? (document.querySelector(".button").innerText += " is starting..")
+    : null;
   isClicked = true;
+});
+
+// question 3
+let form = document.querySelector("form");
+let inputs = form.querySelectorAll("input");
+console.log(inputs);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!inputs[0].value || !inputs[1].value) {
+    alert("form inputs are invalid");
+  } else {
+    alert("form is submitted successfully");
+    inputs[0].value = inputs[1].value  = ""
+  }
 });
