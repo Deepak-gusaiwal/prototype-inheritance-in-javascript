5fa81077c288ab0f944742f18ff6b16896836823
deepak <dgusaiwal537@gmail.com>
2024-03-14 21:59:18 +0530

    start scroll trigger

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 let tl = gsap.timeline();
 
-tl.from(['header .wrapper .logo','header .wrapper nav a'],{
-    y:"-100%",
-    delay:0.2,
-    stagger:0.2,
-    duration:0.4
+tl.from(['header .wrapper .logo', 'header .wrapper nav a'], {
+    y: "-100%",
+    delay: 0.2,
+    stagger: 0.2,
+    duration: 0.4
 })
-tl.from(['.heroContentContainer .bound h2'],{
-    y:"100%",
-    delay:-0.8,
-    stagger:0.2,
-    duration:0.4
+tl.from(['.heroContentContainer .bound h2'], {
+    y: "100%",
+    delay: -0.8,
+    stagger: 0.2,
+    duration: 0.4
 })
-tl.from(['.hero .wrapper>img.bg'],{
-    rotate:"260deg",
-    opacity:0,
-    duration:0.5
+tl.from(['.hero .wrapper>.bg'], {
+    rotate: "260deg",
+    opacity: 0,
+    duration: 0.5
+})
+gsap.from('.about .wrapper h2', {
+    rotate: "260deg",
+    duration: 2,
+    // scrollTrigger: ".about .wrapper h2"
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".about .wrapper h2",
+        markers:true,
+        start:"top 60%",
+        end:"top 20%",
+        scrub:2
+    }
 })
