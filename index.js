9c4a05d6e0145708e70c3c7bc7ef139bcd2a2915
deepak <dgusaiwal537@gmail.com>
2024-03-14 20:25:53 +0530

    learn about timeline in gsap

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // start learning gsap
+const tl = gsap.timeline()
+tl.to("#box1",{
+    x:500,
+    rotate:'360deg',
+    duration:1,
+    delay:1,
+    backgroundColor:"crimson"
+    
+})
+tl.to("#box2",{
+    x:500,
+    rotate:'360deg',
+    duration:1,
+    backgroundColor:"crimson"
+    
+})
+tl.to("#box3",{
+    x:500,
+    rotate:'360deg',
+    duration:1,
+    backgroundColor:"crimson"
+    
+})
