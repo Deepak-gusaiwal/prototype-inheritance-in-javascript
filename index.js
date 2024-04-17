95a68086871921a5e5fad04f4e598631a631a760
Deepak Home <dgusaiwal537@gmail.com>
2024-04-17 17:15:54 +0530

    add animation on active slide

--- index.js
+++ index.js
@@ -33,45 +33,66 @@ const slideBoxes = document.querySelectorAll(".slideBox");
 
 // Set initial slide index
 let currentSlideIndex = 0;
-
+let isAnimating = false;
 // Function to show a slide
-function showSlide(index,isNext=true) {
-  // Hide all slides
+function showSlide(index, isNext = true) {
+  isAnimating = true;
+  const currentSlide = slideBoxes[index];
+  // Hide all slides except the current one
   slideBoxes.forEach((slideBox, i) => {
     if (i !== index) {
       gsap.set(slideBox, {
         autoAlpha: 0,
       }); // Hide the slide
     }
-    if (i == index) {
-      gsap.from(slideBox, {
-        y: isNext ? "100":"-100",
-        duration: 0.2,
-        opacity:0
-      });
-    }
   });
 
   // Show the current slide
-  gsap.set(slideBoxes[index], { autoAlpha: 1 });
+  gsap.set(currentSlide, { autoAlpha: 1 });
+  const image = currentSlide.querySelector(".imgBox");
+  const headings = currentSlide.querySelectorAll("h2");
+  let tl = gsap.timeline({
+    onComplete: function () {
+      isAnimating = false;
+    },
+  });
+  tl.from(image, {
+    y: isNext ? 100 : -100,
+    duration: 0.5,
+    opacity: 0,
+  });
+  tl.from(headings, {
+    x: isNext ? 100 : -100,
+    duration: 0.2,
+    delay: -0.5,
+    opacity: 0,
+  });
 }
 
 // Function to move to next slide
 function nextSlide() {
+  if (isAnimating) return;
   currentSlideIndex = (currentSlideIndex + 1) % slideBoxes.length;
   showSlide(currentSlideIndex);
 }
 
 // Function to move to previous slide
 function prevSlide() {
+  if (isAnimating) return;
   currentSlideIndex =
     (currentSlideIndex - 1 + slideBoxes.length) % slideBoxes.length;
-  showSlide(currentSlideIndex,false);
+  showSlide(currentSlideIndex, false);
 }
 
 // Show the initial slide
 showSlide(currentSlideIndex);
 
+function autoPlay(time="5000") {
+  setInterval(() => {
+    nextSlide();
+  }, time);
+}
+autoPlay();
 // Event listeners for slide controls
 document.getElementById("left").addEventListener("click", prevSlide);
 document.getElementById("right").addEventListener("click", nextSlide);
